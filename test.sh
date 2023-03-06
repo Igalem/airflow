f0670b138286ebc0f836348a79dfcf4a180aece0
Igalem <Qwe123!!>
2023-03-06 17:50:35 +0200

    remove_buck

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 export SQLALCHEMY_SILENCE_UBER_WARNING=1
 export PYTHONPATH=/Users/igale/vsCode/airflow
 export AIRFLOW_HOME=/Users/igale/vsCode/airflow
-export S3_BUCKET_NAME='kenshoo-lab-airflow-dev'
+export S3_BUCKET_NAME='xx-xx-xx-dev'
 export AIRFLOW_GPL_UNIDECODE=True
 export AWS_DEFAULT_REGION=us-east-1
 export AIRFLOW__SCHEDULER__SCHEDULE_AFTER_TASK_EXECUTION=False
